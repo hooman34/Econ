48ec80683d442258d8e422a9224a4de28823c912
hooman34 <gieun1505@gmail.com>
2021-04-13 01:08:21 +0900

    remove portfolio analysis

--- econ_platform/utils/utils.py
+++ econ_platform/utils/utils.py
@@ -14,7 +14,7 @@ import requests
 # auth
 basepath = Path(__file__).parent.parent
 
-with open(str(basepath)+'/keys/keys.json', 'r') as key_file:
+with open(str(basepath) + '/keys/keys.json', 'r') as key_file:
     keys = json.load(key_file)
 
 fred = Fred(api_key=keys['fred'])
@@ -22,18 +22,20 @@ quandl.ApiConfig.api_key = keys['quandl']
 
 
 def fundamental_metric(soup, metric):
-    return soup.find(text = metric).find_next(class_='snapshot-td2').text
+    return soup.find(text=metric).find_next(class_='snapshot-td2').text
+
 
 def get_fundamental_data(df):
     for symbol in df.index:
         try:
             url = ("http://finviz.com/quote.ashx?t=" + symbol.lower())
-            soup = bs(requests.get(url).content) 
-            for m in df.columns:                
-                df.loc[symbol,m] = fundamental_metric(soup,m)                
+            soup = bs(requests.get(url).content)
+            for m in df.columns:
+                df.loc[symbol, m] = fundamental_metric(soup, m)
         except Exception as e:
-            print (symbol, 'not found')
+            print(symbol, 'not found')
     return df
+
 
 def convert_date_format(d, format):
     """"
@@ -43,21 +45,23 @@ def convert_date_format(d, format):
     y, m, d = d.split('-')
 
     if format == 'Fred':
-        return m+'/'+d+'/'+y
+        return m + '/' + d + '/' + y
     elif format == 'YMD':
-        return y+'-'+m+'-'+d
+        return y + '-' + m + '-' + d
     elif format == 'Investing.com':
-        return d+'/'+m+'/'+y
+        return d + '/' + m + '/' + y
+
 
 def fred_quandl(indx, start_date, end_date):
     """
     indx
     start_date, end_date: 'YYYY-MM-DD'
     """
-    df = quandl.get('FRED/'+indx, start_date=start_date, end_date=end_date)
+    df = quandl.get('FRED/' + indx, start_date=start_date, end_date=end_date)
     df = df.reset_index()
     df.columns = ['Date', indx]
     return df
+
 
 def fred_fred(code, column_name='v', observation_start=None, observation_end=None):
     """
@@ -68,53 +72,31 @@ def fred_fred(code, column_name='v', observation_start=None, observation_end=Non
     df.columns = ['date', column_name]
     return df
 
+
 def investing_api(call_type, ticker, from_date, to_date, country='united states'):
     """
     call_type: etf, stock, fund, index
     ticker: str. ticker name
     from_date: dd/mm/yyyy
     """
-    if call_type=='etf':
+    if call_type == 'etf':
         # search name from ticker and return
         etfs = investpy.etfs.get_etfs(country=country)
-        etf_name = etfs.loc[(etfs.symbol==ticker), 'name'].tolist()[0]
+        etf_name = etfs.loc[(etfs.symbol == ticker), 'name'].tolist()[0]
         data = investpy.get_etf_historical_data(etf=etf_name, country=country,
                                                 from_date=from_date,
                                                 to_date=to_date).reset_index()
-    if call_type=='stock':
+    if call_type == 'stock':
         data = investpy.stocks.get_stock_historical_data(stock=ticker, country=country,
-                                                        from_date=from_date,
-                                                        to_date=to_date).reset_index()
+                                                         from_date=from_date,
+                                                         to_date=to_date).reset_index()
     return data
 
-def seekingAlpha_estimates(ticker, data_type, period_type='annual', key=keys['rapidAPI_seekingalpha']):
-    
-    assert data_type in ['eps', 'revenues']
-    assert period_type in ['quarterly','annual']
-    url = "https://seeking-alpha.p.rapidapi.com/symbols/get-estimates"
-
-    querystring = {"symbol":ticker.lower(),
-                   "data_type":data_type,
-                   "period_type":period_type}
-    headers = {
-        'x-rapidapi-key': key,
-        'x-rapidapi-host': "seeking-alpha.p.rapidapi.com"
-        }
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    df = pd.DataFrame([response.json()['data'][i]['attributes'] for i in range(len(response.json()['data']))])
-
-    df['average'] = df['actual']
-    df.loc[df['actual'].isna(), 'average'] = df.loc[df['actual'].isna(), 'consensus']
-    df['average'] = df['average'].mean()
-
-    return df
 
 def _plot_two_data(plot_type1, data1, x1, y1, plot_type2, data2, x2, y2):
-
     trace1 = _create_trace(plot_typd1, data1, x1, y1)
     trace2 = _create_trace(plot_typd2, data2, x2, y2)
-    
+
     fig = make_subplots(specs=[[{"secondary_y": True}]])
     fig.add_trace(trace1)
     fig.add_trace(trace2, secondary_y=True)
@@ -129,16 +111,17 @@ def _plot_two_data(plot_type1, data1, x1, y1, plot_type2, data2, x2, y2):
 
     return fig
 
+
 def _create_trace(plot_type, data, x, y):
     """
     create trace
     """
-    if plot_type=='bar':
+    if plot_type == 'bar':
         trace = go.Bar(x=data[x],
                        y=data[y],
                        name=y)
-    if plot_type=='line':
+    if plot_type == 'line':
         trace = go.Scatter(x=data[x],
-                       y=data[y],
-                       name=y)
+                           y=data[y],
+                           name=y)
     return trace
